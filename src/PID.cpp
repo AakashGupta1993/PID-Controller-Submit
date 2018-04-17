df22dd18238ab2d4dc60e3b99efc0c091a1ff682
Aakash <aakash_gupta93@yahoo.com>
2018-04-17 16:47:55 +0530

    Changes 1

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,16 +6,35 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+	Kp = 0;
+	Ki = 0;
+	Kd = 0;
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
+}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+	
+  //double prev_cte = 0.0;
+  //double int_cte = 0;
+  d_error = cte - p_error;	
+  p_error = cte;
+  i_error += cte;
+
 }
 
 double PID::TotalError() {
+	double steer = -Kp*p_error - Kd*d_error - Ki*i_error;
+  return steer;
 }
 
